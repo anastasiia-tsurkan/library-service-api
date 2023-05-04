1aa0dd2a9d0e8d94376d1e7ac0d5672f10190d6c
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-04 12:20:26 +0300

    Create Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return = models.DateField()
+    actual_return = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.user.email}: {self.book.title} -> {self.expected_return}"

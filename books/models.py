4979877e694866976e9ffa4e32ae5ba97c180fc1
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-04 11:49:37 +0300

    Change inventory property

--- books/models.py
+++ books/models.py
@@ -9,8 +9,9 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=50, choises=CoverChoices.choices)
+    amount = models.IntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
     @property
     def inventory(self):
-        return Book.objects.count()
+        return self.amount

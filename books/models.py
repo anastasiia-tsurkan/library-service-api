62d0225b0b84ce97b36f164c6a540e3f30a5d359
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-05 12:36:36 +0300

    Fix typo in choices

--- books/models.py
+++ books/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=50, choises=CoverChoices.choices)
+    cover = models.CharField(max_length=50, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 

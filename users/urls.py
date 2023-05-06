13214a51a76dbbb1582b5eb1e9bdb7c9980ff927
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-06 21:03:45 +0300

    Add endpoint user detail

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from users.views import ManageUserView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "users"

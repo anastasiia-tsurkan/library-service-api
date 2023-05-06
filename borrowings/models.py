09709b700b589beee36f631bdb6b93f90b221297
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-06 21:01:42 +0300

    Add new migration (blank=True borrowing fields)

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,7 +7,7 @@ from books.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return = models.DateField()
-    actual_return = models.DateField()
+    actual_return = models.DateField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
     user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
 

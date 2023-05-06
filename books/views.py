fe8e51f50010c16c4fecc1ab789e71b8ebf87e71
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-06 21:05:00 +0300

    Add get_permissions method

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -7,3 +8,10 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]

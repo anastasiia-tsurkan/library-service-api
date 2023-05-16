4f60d4b6d08f36c62ba57529c2b1effab33d0112
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-05-16 13:13:14 +0300

    Add register and refresh endpoints

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
